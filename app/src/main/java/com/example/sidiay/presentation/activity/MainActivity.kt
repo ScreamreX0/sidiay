09c34a64a10912c2fd0ddd452868b428ad3c0cbf
scx <scx0503@gmail.com>
2022-12-07 19:43:27 +0300

    main menu

--- app/src/main/java/com/example/sidiay/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/sidiay/presentation/activity/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.sidiay.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.sidiay.R
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // THEME
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // LANGUAGE
+        Locale.setDefault(Locale("ru_ru"))
     }
 }
