1004133ab123073a5aca990af363453ef2e9f790
scx <scx0503@gmail.com>
2022-12-17 21:28:27 +0300

    -

--- app/src/main/java/com/example/sidiay/presentation/fragments/menu/MainMenuFragment.kt
+++ app/src/main/java/com/example/sidiay/presentation/fragments/menu/MainMenuFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -41,16 +39,30 @@ class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
     }
 
     private fun bottomNavOnClickListener() {
-        val bottomNavigationView = view?.findViewById<BottomNavigationView>(R.id.f_main_bottom_nav)
-        val navController = (childFragmentManager.findFragmentById(R.id.f_main_fragment_container) as NavHostFragment)
-            .navController
-        bottomNavigationView?.setupWithNavController(navController)
+        val navController =
+            (childFragmentManager.findFragmentById(R.id.f_main_fragment_container) as NavHostFragment)
+                .navController
+
+        binding.fMainBottomNav.setOnItemSelectedListener {
+            val builder: NavOptions.Builder = NavOptions.Builder()
+            builder.setEnterAnim(androidx.navigation.ui.R.anim.nav_default_enter_anim)
+                .setPopEnterAnim(androidx.navigation.ui.R.anim.nav_default_pop_enter_anim)
+                .setExitAnim(androidx.navigation.ui.R.anim.nav_default_exit_anim)
+                .setPopExitAnim(androidx.navigation.ui.R.anim.nav_default_pop_exit_anim)
+            val options: NavOptions = builder.build()
+
+            navController.navigate(it.itemId, args.toBundle(), options)
+
+            return@setOnItemSelectedListener true
+        }
     }
 
     private fun initFragmentContainer() {
         return
-        findNavController().navigate(MainMenuFragmentDirections.actionMainMenuFragmentToNavGraphHome(
-            args.user
-        ))
+        findNavController().navigate(
+            MainMenuFragmentDirections.actionMainMenuFragmentToNavGraphHome(
+                args.user
+            )
+        )
     }
 }
